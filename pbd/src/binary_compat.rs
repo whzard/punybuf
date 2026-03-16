9ed24cbf8e0f9f539740f921c380a635cf5c2516
whezard <90904039+whzard@users.noreply.github.com>
2026-03-16 13:00:49 +0100

    [pbd] fixed broken build :(

--- pbd/src/binary_compat.rs
+++ pbd/src/binary_compat.rs
@@ -1,9 +1,17 @@
 use crate::{errors::PunybufError, flattener::PunybufDefinition};
 
-pub struct BinaryCompat;
+pub struct BinaryCompat<'a> {
+	prev_json: &'a str,
+	next: &'a PunybufDefinition,
+}
 
-impl BinaryCompat {
-	pub fn check(&self, prev_json: &str, next: &PunybufDefinition) -> Result<(), PunybufError> {
+impl<'a> BinaryCompat<'a> {
+	pub fn new(prev_json: &'a str, next: &'a PunybufDefinition) -> Result<Self, String> {
+		Ok(Self {
+			prev_json, next
+		})
+	}
+	pub fn check(&self) -> Result<(), PunybufError> {
 		
 		Ok(())
 	}
